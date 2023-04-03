a13a93c0a1c0d77a35d87f8136541b1e05fc02da
micate <micate@me.com>
2023-04-03 10:14:08 +0800

    fix: typo

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 class LoadChunkStyleNextToTagWebpackPlugin {
   name = 'load-chunk-style-next-to-tag';
 
-  constructor({ tag }) {
+  constructor({ group, tag }) {
+    this.group = group;
     this.tag = tag;
   }
 
+  getTagQueryCode() {
+    const { tag } = this;
+
+    if (tag) {
+      const tags = Array.isArray(tag) ? tag.map((t) => (t ? t.trim() : '')).filter((t) => t) : [tag];
+      return tags.map((t) => `document.querySelector('${t}')`).join(' || ');
+    }
+  }
+
   apply(compiler) {
-    const { name } = this;
+    const { group, name } = this;
     const pluginName = `${name}-webpack-plugin`;
 
-    let query;
-    if (this.tag) {
-      const tags = Array.isArray(this.tag) ? this.tag.map((t) => (t ? t.trim() : '')).filter((t) => t) : [this.tag];
-      query = tags.map((t) => `document.querySelector('${t}')`).join(' || ');
-    }
-    if (!query) {
+    const queryCode = this.getTagQueryCode();
+    if (!queryCode) {
       console.warn(pluginName, 'tag options is empty');
       return;
     }
@@ -22,19 +28,23 @@ class LoadChunkStyleNextToTagWebpackPlugin {
     function replace(tagName) {
       return `
 var attrName = '${name}';
-${tagName}.setAttribute(attrName, chunkId);
+var groupName = '${group || ''}';
+${tagName}.setAttribute(attrName, groupName || chunkId);
 
-var chunkLinks = document.querySelectorAll('link[' + attrName + ']');
+var chunkSelector = groupName ? ('link[' + attrName + '="' + groupName + '"]') : ('link[' + attrName + ']');
+var chunkLinks = document.querySelectorAll(chunkSelector);
+
 var targetTag;
 if (chunkLinks && chunkLinks.length) {
   targetTag = chunkLinks[chunkLinks.length - 1];
 } else {
   try {
-    targetTag = ${query};
+    targetTag = ${queryCode};
   } catch(e) {
     console.error(attrName, 'query target tag failed', e);
   }
 }
+
 if (targetTag && targetTag.nextSibling) {
   targetTag.parentNode.insertBefore(${tagName}, targetTag.nextSibling);
 } else {
@@ -47,13 +57,13 @@ if (targetTag && targetTag.nextSibling) {
       compilation.mainTemplate.hooks.requireEnsure.tap(pluginName, (source) => {
         // webpack v4
         const keywordV4 = 'document.head.appendChild(linkTag);';
-        if (source.contains(keywordV4)) {
+        if (source && source.includes(keywordV4)) {
           return source.replace(keywordV4, replace('linkTag'));
         }
 
         // webpack v5
         const keywordV5 = 'document.head.appendChild(link);';
-        if (source.contains(keywordV5)) {
+        if (source && source.includes(keywordV5)) {
           return source.replace(keywordV5, replace('link'));
         }
 
